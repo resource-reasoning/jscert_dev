ab66d4818d10fcfd82c6095c88227e653c426c7a
Conrad <conrad.watt12@imperial.ac.uk>
2014-08-31 13:00:57 +0100

    Lay groundwork for strict mode early error reporting in eval. Add esprima.js for change tracking.

--- simple_print.js
+++ simple_print.js
@@ -1,10 +1,12 @@
 var esprima = require('esprima');
 var fs = require('fs');
 var filename = process.argv[2];
+var force_strict = process.argv[3];
 fs.readFile(filename, "utf8", f);
 
 function f(err, data) {
-  var out = JSON.stringify(esprima.parse(data, {range: true}), null, 4);
+  var strict = (force_strict === "-force_strict");
+  var out = JSON.stringify(esprima.parse(data, {range: true, force_strict: strict}), null, 4);
   var output = filename.substring(0, (filename.length - 3)) + ".json";
   fs.writeFile(output, out, [], null);
 }
