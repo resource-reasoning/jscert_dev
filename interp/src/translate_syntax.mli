8d124871257ada7965f67aeecb95ea1344a4e4c5
Thomas Wood <tw1509@doc.ic.ac.uk>
2015-01-13 17:28:22 +0000

    Refine interface to Translate_syntax

--- interp/src/translate_syntax.mli
+++ interp/src/translate_syntax.mli
@@ -1,22 +1,12 @@
-
 exception CoqSyntaxDoesNotSupport of string
 exception Empty_list
 
 val string_to_coq : string -> char list
 
-val unary_op_to_coq : Parser_syntax.unary_op -> JsSyntax.unary_op
-val arith_op_to_coq : Parser_syntax.arith_op -> JsSyntax.binary_op
-val bin_op_to_coq : Parser_syntax.bin_op -> JsSyntax.binary_op
-
-val exp_to_literal : Parser_syntax.exp -> JsSyntax.literal
-
-val exp_to_exp : Parser_syntax.exp -> JsSyntax.expr
-val exp_to_stat : Parser_syntax.exp -> JsSyntax.stat
+(* Parser ast into coq ast *)
 val exp_to_prog : Parser_syntax.exp -> JsSyntax.prog
-val exp_to_elem : Parser_syntax.exp -> JsSyntax.element
-val exp_to_funcbody :
-  Parser_syntax.exp -> JsSyntax.strictness_flag -> JsSyntax.funcbody
 
+(* Call into parser then translate into Coq ast *)
 val coq_syntax_from_file : string -> JsSyntax.prog
 val coq_syntax_from_string : string -> JsSyntax.prog
 
