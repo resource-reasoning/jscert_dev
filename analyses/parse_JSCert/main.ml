55d188e2102a0fe50812c04ea089efd3a3f2d6f3
Martin Bodin <martin.bodin@ens-lyon.org>
2015-01-26 15:10:07 +0000

    Starting to extract Coq files.

--- analyses/parse_JSCert/main.ml
+++ analyses/parse_JSCert/main.ml
@@ -1,45 +1,56 @@
 
 open Coq_repr
-
-let rec variable_used x = function
-    | Ident (i, m, y) ->
-        m = None && y = x
-    | App (e1, internal, e2) ->
-        variable_used x e1 || variable_used x e2
-    | Binop (op, e1, e2) ->
-        variable_used x e1 || variable_used x e2
-    | Unop (op, e) ->
-        variable_used x e
-    | Couple (e1, e2) ->
-        variable_used x e1 || variable_used x e2
-    | String s ->
-        false
-    | Int i ->
-        false
-    | Forall (l, e) ->
-        not (List.exists (fun (y, _) -> x = y) l) && variable_used x e
-    | Match (e, l) ->
-        variable_used x e
-            || List.exists (fun (p, e) ->
-                    not (variable_used x p) && variable_used x e) l
+open Coq_repr_aux
 
 let _ =
+    (*****************************************)
     print_endline "Reading rules." ;
     let lexbuf = Lexing.from_channel (open_in "../../coq/JsPrettyRules.v") in
-    let f = Parser.main Lexer.token lexbuf in
+    let jsprettyrulesfile = Parser.main Lexer.token lexbuf in
+    (*****************************************)
     print_endline "Basic checks on rules." ;
     let check = function
         | File_reductions rl ->
             let checkred r =
                 let checkrule r =
                     List.iter (fun (x, _) ->
-                        if not (List.exists (variable_used x) r.rule_statement_list)
-                        then prerr_endline ("Warning: parameter " ^ x ^ " seems to be unused in Rule " ^ r.rule_name ^ ".")) r.rule_params
+                        let var_used =
+                            List.exists (variable_used x) r.rule_statement_list in
+                        let var_used_in_types =
+                            List.exists (function
+                                | (_, None) -> false
+                                | (_, Some t) -> variable_used_type x t) r.rule_params in
+                        if not (var_used || var_used_in_types)
+                        then prerr_endline ("Warning: parameter " ^ x ^ " seems to be unused in Rule " ^ r.rule_name ^ ".") ;
+                        if Utils.count (fun (y, _) -> x = y) r.rule_params > 1
+                        then prerr_endline ("Warning: parameter " ^ x ^ " seems to be defined more than once in Rule " ^ r.rule_name ^ ".") ;
+                        ()) r.rule_params
                 in
                 List.iter checkrule r.rules
             in
             List.iter checkred rl
         | _ -> () in
-    List.iter check f ;
+    List.iter check jsprettyrulesfile ;
+    (*****************************************)
+    let imports =
+        List.concat
+            (List.map (function File_import s -> [s] | _ -> []) jsprettyrulesfile) in
+    let scopes =
+        List.concat
+            (List.map (function File_scope s -> [s] | _ -> []) jsprettyrulesfile) in
+    let implicit_types =
+        List.concat
+            (List.map (function File_implicit_type im -> [im] | _ -> []) jsprettyrulesfile) in
+    let coq_file name =
+        let f = Utils.open_out_coq name in
+        print_endline ("File " ^ name ^ " created and being writen.") ;
+        List.iter (fun s -> Utils.output_endline f ("Require Import " ^ s ^ ".")) imports ;
+        List.iter (fun s -> Utils.output_endline f ("Open Scope " ^ s ^ ".")) scopes ;
+        List.iter (fun (x, t) -> Utils.output_endline f ("Implicit Type " ^ x ^ " : " ^ string_of_type t ^ ".")) implicit_types ;
+        flush f ;
+        f in
+    (*****************************************)
+    print_endline "Extracting rule names." ;
+    let names = coq_file "gen/JsNames.v" in
     ()
 
