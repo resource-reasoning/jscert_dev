88a87aafe7844fd186594f7a94b4e45120c56312
Thomas Wood <tw1509@doc.ic.ac.uk>
2015-05-29 17:01:59 +0100

    Update runtests.py condor help message

--- tools/runtests.py
+++ tools/runtests.py
@@ -1012,36 +1012,49 @@ class Runtests:
         print """
 Condor Help
 
-This script is able to submit test run jobs to Condor, results may only be recorded using the Postgres database option.
+This script is able to submit test run jobs to Condor, results may only be
+recorded using the Postgres database option.
 
-Ensure you have the Condor binaries in your PATH (this is not necessary for this script to run, but is required for use of eg, condor_q)
-  export PATH ${PATH}:${CONDOR_HOME}/bin
-
-You should also ensure that the Condor Python binding libraries are in your PYTHONPATH, eg:
+You require a working Condor installation on the local machine.
+You may need to add the Condor Python binding libraries to your PYTHONPATH, eg:
   export PYTHONPATH=${PYTHONPATH}:${CONDOR_HOME}/lib/python
 
-On Imperial CSG-run machines you will need to set the following environment variables to enable Condor Python support:
-  export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH:/vol/condor/releases/8.0.6/condor-8.0.6-x86_64_Ubuntu12-stripped/lib:/vol/condor/releases/8.0.6/condor-8.0.6-x86_64_Ubuntu12-stripped/lib/condor
-  export PYTHONPATH=/vol/condor/releases/8.0.6/condor-8.0.6-x86_64_Ubuntu12-stripped/lib/python:$PYTHONPATH
-(This is to workaround a broken site-wide Condor installation)
+Imperial DoC users should place the following commands in their shell profile
+to enable Condor support:
+  export PATH=${PATH}:${CONDOR_HOME}/bin
+  export LD_LIBRARY_PATH=${CONDOR_HOME}/lib/condor
+  export PYTHONPATH=${PYTHONPATH}:${CONDOR_HOME}/lib/python
 
-A Condor Python binding import check is executed at the end of this help message.
+A full JSCert (Coq/OCaml) installation is not required for each machine the
+tests are to be run on, you just need a working run_js executable in the interp
+directory. (You should test this on an appropriate machine in the cluster
+before a run). The run_js interpreter uses few shared libraries, so should
+hopefully be portable between Linux distros without need for recompilation.
 
-A full JSCert (Coq/OCaml) installation is not required for each machine the tests are to be run on, you just need a working run_js executable in the interp directory. (You should test this on an appropriate machine in the cluster before a run). The run_js interpreter uses few shared libraries, so should hopefully be portable between Linux distros without need for recompilation.
-
-A Postgres database is required to collect results. Options as printed by --help should be straightforward. A template Postgres configuration file is available in the repo at /.pgconfig.example
-If you've forgotten them, your Postgres username and password are usually kept in the .pgpass file in your home directory, this testrunner makes no attempt to read this file.
+A Postgres database is required to collect results. Options as printed by
+--help should be straightforward. A template Postgres configuration file is
+available in the repo at /.pgconfig.example
+If you've forgotten them, your Postgres username and password are usually kept
+in the .pgpass file in your home directory, this testrunner makes no attempt to
+read this file.
 
 Sample command line to run tests on Condor:
   runtests.py --condor --db postgres --batch_size 4 tests/test262/
 
-Note that for large test suites, such as test262, the Condor scheduler daemon runs out of memory with >~5000 jobs. The batch size parameter groups multiple (4 in this case) test cases into one Condor job to prevent an explosion in memory. The memory use results from the way this script passes parameters to Condor and the way Condor stores them, we should probably fix this...
+Note that for large test suites, such as test262, the Condor scheduler daemon
+runs out of memory with >~5000 jobs. The batch size parameter groups multiple
+(4 in this case) test cases into one Condor job to prevent an explosion in
+memory. The memory use results from the way this script passes parameters to
+Condor and the way Condor stores them, we should probably fix this...
 
-The status of Condor jobs can be retrieved using the condor_q command. Jobs that have become stuck can be removed using condor_rm.
+The status of Condor jobs can be retrieved using the condor_q command. Jobs
+that have become stuck can be removed using condor_rm.
 
-Presently, the only way to interrogate the results is to perform SQL queries by hand. The analysis scripts haven't yet been updated to support the new database schema.
+Presently, the only way to interrogate the results is to perform SQL queries by
+hand. The analysis scripts haven't yet been updated to support the new database
+schema.
 """
-        print "Testing Condor Python bindings: ",
+        print "Testing Condor Python bindings: "
         self.condor_test_import()
         print "OK!"
 
