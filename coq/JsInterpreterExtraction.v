resource-reasoning/jscert_dev

commit b5504e0c3eac70a8c946ff00e27cb5331608bbaa

Remove dependency on Prheap for jsjsref

coq/JsInterpreterExtraction.v
@@ -232,16 +232,9 @@ Extract Constant parse_pickable => "(fun s strict ->
 
 
 (* Debugging *)
-Extract Inlined Constant not_yet_implemented_because => "(fun s ->
-  print_endline (__LOC__ ^ "": Not implemented because: "" ^ Prheap.string_of_char_list s) ;
-  Coq_result_not_yet_implemented)".
-Extract Inlined Constant impossible_because => "(fun s ->
-  print_endline (__LOC__ ^ "": Stuck because: "" ^ Prheap.string_of_char_list s) ;
-  Coq_result_impossible)".
-Extract Inlined Constant impossible_with_heap_because => "(fun s message ->
-  print_endline (__LOC__ ^ "": Stuck!\nState:  "" ^ Prheap.prstate true s
-    ^ ""\nMessage:\t"" ^ Prheap.string_of_char_list message) ;
-  Coq_result_impossible)".
+Extract Inlined Constant not_yet_implemented_because => "(fun s -> Debug.not_yet_implemented_because __LOC__ s; Coq_result_impossible)".
+Extract Inlined Constant impossible_because => "(fun s -> Debug.impossible_because __LOC__ s; Coq_result_impossible)".
+Extract Inlined Constant impossible_with_heap_because => "(fun s m -> Debug.impossible_with_heap_because __LOC__ s m; Coq_result_impossible)".
 
 
 (* Final Extraction *)

interp/src/debug.ml
@@ -1,0 +1,37 @@
+open Prheap
+
+(* Various debug helper functions *)
+
+(* Inserted by extraction *)
+
+let not_yet_implemented_because loc s =
+  print_endline (loc ^ ": Not implemented because: " ^ string_of_char_list s)
+
+let impossible_because loc s =
+  print_endline (loc ^ ": Stuck because: " ^ string_of_char_list s)
+
+let impossible_with_heap_because loc s message =
+  print_endline (loc ^ ": Stuck!\nState:  " ^ prstate true s ^ "\nMessage:\t" ^ string_of_char_list message)
+
+(* Inserted into JsInterpreter by patching in Makefile functions named after the appropriate insertion *)
+
+let ref_get_value r =
+  prerr_string "Warning: ref_get_value returns the undefined value on ";
+  prerr_string (Prheap.prref r);
+  prerr_newline()
+
+
+let ref_get_value_2 r =
+  prerr_string ("Warning: ref_get_value returns the undefined value on ");
+  prerr_string (Prheap.prresvalue r);
+  prerr_newline()
+
+let run_object_method l =
+  prerr_endline (Printf.sprintf "Warning: in run_object_method the location %s is unfetchable." (Prheap.prloc l))
+
+let run_object_heap_set_extensible l =
+  prerr_endline (Printf.sprintf "Warning: in run_object_heap_set_extensible the location %s is unfetchable." (Prheap.prloc l))
+
+let lexical_env_get_identifier_ref x0 =
+  prerr_endline ("Warning: lexical_env_get_identifier_ref fails to find " ^ (Prheap.string_of_char_list x0))
+

interp/src/insert/lexical_env_get_identifier_ref
@@ -1,2 +1,2 @@
-    prerr_endline (\"Warning: lexical_env_get_identifier_ref fails to find \" ^ (Prheap.string_of_char_list x0));\n
+    Debug.lexical_env_get_identifier_ref x0;\n
     result_val s (ref_create_value (Coq_value_prim Coq_prim_undef) x0 str)

interp/src/insert/ref_get_value
@@ -1,1 +1,1 @@
-prerr_string (\"Warning: ref_get_value returns the undefined value on \"); prerr_string (Prheap.prref r); prerr_newline(); res_res (run_error s Coq_native_error_ref)
+Debug.ref_get_value r; res_res (run_error s Coq_native_error_ref)

interp/src/insert/ref_get_value2
@@ -2,7 +2,7 @@ let ref_get_value runs s c r =\n
   let res = ref_get_value runs s c r in match res with\n
     \| JsInterpreterMonads.Coq_result_some (Coq_specret_val (_, rs)) -> begin match rs with\n
         \| Coq_value_prim (Coq_prim_undef) ->\n
-          prerr_string (\"Warning: ref_get_value returns the undefined value on \"); prerr_string (Prheap.prresvalue r); prerr_newline(); res\n
+          Debug.ref_get_value_2 r; res\n
         \| _ -> res\n
       end\n
     \| _ -> res\n

interp/src/insert/run_object_heap_set_extensible
@@ -1,7 +1,7 @@
 let run_object_heap_set_extensible b s l =\n
   let opt = object_binds_pickable_option s l in\n
     begin match opt with\n
-      \| None -> prerr_endline (Printf.sprintf \"Warning: in run_object_heap_set_extensible the location \%s is unfetchable.\" (Prheap.prloc l))\n
+      \| None -> Debug.run_object_heap_set_extensible l\n
       \| _ -> ()\n
     end;\n
     LibOption.map (fun o -> object_write s l (object_set_extensible o b)) opt\n

interp/src/insert/run_object_method
@@ -1,7 +1,7 @@
 let run_object_method proj s l =\n
   let opt = object_binds_pickable_option s l in\n
     begin match opt with\n
-      \| None -> prerr_endline (Printf.sprintf \"Warning: in run_object_method the location \%s is unfetchable.\" (Prheap.prloc l))\n
+      \| None -> Debug.run_object_method l\n
       \| _ -> ()\n
     end;\n
     LibOption.map proj opt\n

jsjsref/generator/Makefile
@@ -73,7 +73,6 @@ all: everything
 
 CC          := ocamlc -c
 OCAMLDEP    := ocamldep -one-line
-DEPSED      := sed -e "s/cmx/cmi/; s/cmx/cmi/g"
 OCAMLBUILD := ocamlbuild -j 4 -classic-display -use-ocamlfind -X tests -X $(STDLIB_DIR)
 
 GENERATOR := ./main.byte
@@ -106,23 +105,15 @@ $(STDLIB_DIR)/stdlib.cmi: $(STDLIB_DIR)/stdlib.mli
 
 ##### Rule for dependencies
 
-tests/%.mli.d: tests/%.mli
-	$(OCAMLDEP) -I $(<D) $< | $(DEPSED) > $@
-
-tests/%.ml.d: tests/%.ml
-	$(OCAMLDEP) -I $(<D) $< | $(DEPSED) > $@
+tests/%.d: tests/%
+	$(OCAMLDEP) -all -I $(<D) $< > $@
 
 ##### Rule for cmi
 
 tests/%.cmi: tests/%.ml main.byte stdlib 
 	./main.byte -mode cmi -I $(<D) $<
 
-##### Custome cmi rules for compilation of mli files without ml source
-
-$(JSREF_PATH)/Translate_syntax.cmi: $(JSREF_PATH)/Translate_syntax.mli $(JSREF_PATH)/JsSyntax.cmi stdlib
-	ocamlc -I $(JSREF_PATH) -I stdlib_ml -open Stdlib $<
-
-$(JSREF_PATH)/Prheap.cmi: $(JSREF_PATH)/Prheap.mli stdlib $(JSREF_PATH)/JsSyntax.cmi
+tests/%.cmi: tests/%.mli stdlib
 	ocamlc -I $(JSREF_PATH) -I stdlib_ml -open Stdlib $<
 
 ##### Rule for log/unlog/token

commit ae4f940609f378426117089a59546923267c9158

Fix wrong result error type, introduced by b5504e0

coq/JsInterpreterExtraction.v
@@ -232,7 +232,7 @@ Extract Constant parse_pickable => "(fun s strict ->
 
 
 (* Debugging *)
-Extract Inlined Constant not_yet_implemented_because => "(fun s -> Debug.not_yet_implemented_because __LOC__ s; Coq_result_impossible)".
+Extract Inlined Constant not_yet_implemented_because => "(fun s -> Debug.not_yet_implemented_because __LOC__ s; Coq_result_not_yet_implemented)".
 Extract Inlined Constant impossible_because => "(fun s -> Debug.impossible_because __LOC__ s; Coq_result_impossible)".
 Extract Inlined Constant impossible_with_heap_because => "(fun s m -> Debug.impossible_with_heap_because __LOC__ s m; Coq_result_impossible)".
 